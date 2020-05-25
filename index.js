ae8b7bd6726220eca55a383ce1b837287d108821
binod <kbinod987@gmail.com>
2020-05-26 00:34:20 +0530

    render method

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import React from "react"; //jsx use karne ke liye html tag
 import ReactDOM from "react-dom"; //render method run ke liye
 ReactDOM.render(
-    [
-<h1>hello world!!</h1>,
-<p>hello react</p>,
-<h2>hello jsx</h2>,
+    <React.Fragment>
+<h1>hello world!!</h1>
+<p>hello react</p>
+<h2>hello jsx</h2>
 <h3>successful</h3>
-    ], 
+</React.Fragment>, 
     document.getElementById("root"));//kya dikhana h or kha dikha na h
