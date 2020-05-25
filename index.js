0ed776f538ea0e2d277d075a5648b60c11f32ba9
binod <kbinod987@gmail.com>
2020-05-26 00:21:20 +0530

    render method

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from "react"; //jsx use karne ke liye html tag
+import ReactDOM from "react-dom"; //render method run ke liye
+ReactDOM.render(
+    [
+<h1>hello world!!</h1>,
+<p>hello react</p>,
+<h2>hello jsx</h2>,
+<h3>successful</h3>
+    ], 
+    document.getElementById("root"));//kya dikhana h or kha dikha na h
